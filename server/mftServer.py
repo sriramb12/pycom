1ddafa2bf8c9eb30fffc29437ee67c8ffb3765a6
Sriram Bhamidipati <sriramb@safeai.ai>
2022-04-16 07:04:12 +0530

    email formatting secret file handle generated

--- server/mftServer.py
+++ server/mftServer.py
@@ -4,10 +4,16 @@
 import socket, ssl, pickle
 import threading
 import os
+from os.path import exists
+import random
 import time
 from conf import config
+import string
+import json
+
 class MFTServer:
 	def __init__(self):
+		self.loadRepoDb()
 		certfile = config.get("Settings", "certfile")
 		keyfile = config.get("Settings", "keyfile")
 		self.packetSize = int(config.get("Settings", "packetSize"))
@@ -24,6 +30,7 @@ class MFTServer:
 
 		#create socket object
 		self.srvSocket = socket.socket()
+		self.srvSocket.settimeout(1)
 		#bind host name to socket on pot number
 		self.serverPort = int(config.get("Settings", "icomServerPort"))
 		#socket listening for up to 5 connections
@@ -42,7 +49,30 @@ class MFTServer:
 			self.sockServer.listen()
 		except:
 			exit("listen error")
+	def loadRepoDb(self):
+		self.repoDbFile = config.get("Settings", "repoDbFile")
+		try:
+			with open(self.repoDbFile, 'r') as self.repoDbHandle:
+				self.repoDb = json.load(self.repoDbHandle)
+		except:
+			self.repoDb = {}
+			print("no repo info")
 
+	def saveRepoDb(self):
+		self.repoDbFile = config.get("Settings", "repoDbFile")
+		print("saverepo")
+		jsonObj =  json.dumps(self.repoDb, indent = 4)
+		with open(self.repoDbFile, 'w') as self.repoDbHandle:
+			self.repoDbHandle.write(jsonObj)
+
+	def genHandle(self):
+		char_set = string.ascii_uppercase
+		num_set = string.digits
+		while True:
+			self.handle = ''.join(random.sample(char_set*4, 4)) + ''.join(random.sample(num_set*4, 4)) +'X'
+			if not exists(self.repository + self.handle):
+				return self.handle
+			print("handle" , self.handle)
 	def decryptFile(fileName,key):
 		f = Fernet(key)
 		file_data= ""
@@ -84,25 +114,34 @@ class MFTServer:
 			
 		
 	def recvFile(self, stream):
-		fname = self.repository + self.header["fileName"].split('/')[-1]
+		self.genHandle()
+		fname = self.repository + self.handle
 		f = open(fname, 'wb')
-		print("recv file ", fname)
-		while True:
+		sz = int(self.header['fileSize'])
+		print("recv file  ", fname, sz)
+		rx = 0
+		while rx < sz: 
 			try:
 				data = stream.recv(self.packetSize)
-				print("rx", len(data))
 				f.write(data)
+				rx += len(data)
+				print("rx:", rx)
 			except:
 				#write data from stream.recv(..) to file
 				break
-			if not len(data):
-				f.close()
-				break
+		self.repoDb[self.handle] = self.header
+		f.close()
+		self.saveRepoDb()
 		print('End Of File received, closing connection...')
 		print('-----------------------------------------\n')
+		d = {'filehandle': self.handle}
+		msg = pickle.dumps(d)
+		stream.send(msg)
 		
+	def dumpRepo(self):
+		print(self.repoDb)
 	def handleClient(self, stream):
-		cmds = ['get', 'put', 'query']
+		cmds = ['get', 'put', 'query', 'debug']
 		MAX_SZ=1024*1024
 		msg = stream.recv()
 		print(len(msg))
